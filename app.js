f8d8bfc501ebe42674f1b97ad3edb8f13394b5e5
AbdulJH <abdul.chs21@gmail.com>
2022-05-01 15:45:23 -0500

    Added a function where the computer randomly chooses rock,paper, or scissors | Added another function that checks if the user won, lose, or tied with the computer and shows what they each picked.

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 
 function computerPlay() {
-  let myChoices = ["Rock", "Paper", "Scissors"];
+  let myChoices = ["rock", "paper", "scissors"];
   let randomItem = myChoices[Math.floor(Math.random()*myChoices.length)];
-
-  console.log(randomItem);
+  return randomItem;
 }
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
 
 
+const playerSelection = prompt("Rock, paper, or scissors");
+const computerSelection = computerPlay();
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "Tie!";
+  }
+  if (playerSelection === "rock" && computerSelection === "scissors")  {
+    return "You win! Rock beats scissors";
+  } else if (playerSelection === "rock" && computerSelection !== "scissors") {
+    return "You Lose! "+computerSelection+" beats rock";
+  }
+
+  if (playerSelection === "paper" && computerSelection === "rock")  {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection !== "rock") {
+    return "You Lose! "+computerSelection+" beats paper";
+  }
+
+  if (playerSelection === "scissors" && computerSelection === "paper")  {
+    return "You win! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection !== "paper") {
+    return "You Lose! "+computerSelection+" beats scissors";
+  }
+}
+
+ console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+  
+ 
