5d0401bdec9b99a7f102177c9a6821bbc56c32cf
AbdulJH <abdul.chs21@gmail.com>
2022-05-01 15:00:24 -0500

    Added the external js into the html file || also added function in JS that returns random rps value

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function computerPlay() {
+  let myChoices = ["Rock", "Paper", "Scissors"];
+  let randomItem = myChoices[Math.floor(Math.random()*myChoices.length)];
+
+  console.log(randomItem);
+}
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+
+
+
